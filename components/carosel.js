1644d926b63759ee93b2f21a648d9d16f2f04ed4
moddamatt <echomatt95@gmail.com>
2019-11-10 15:27:02 -0600

    Back to html

---
+++ components/carosel.js
@@ -0,0 +1,9 @@
+// setsup the carosel
+$(document).ready(function() {
+  $(".carosel").slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    arrows: false,
+  });
+});
