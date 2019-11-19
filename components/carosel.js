d547e2adbebb4ef3b0fa4bb04a87cb8020e755dc
moddamatt <echomatt95@gmail.com>
2019-11-19 11:19:32 -0600

    Project section complete.

--- components/carosel.js
+++ components/carosel.js
@@ -1,9 +1,36 @@
 // setsup the carosel
-$(document).ready(function() {
-  $(".carosel").slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    arrows: false,
-  });
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider-nav"
+});
+
+$(".slider-nav").slick({
+  asNavFor: ".slider-for",
+  centerMode: true,
+  focusOnSelect: true,
+  arrows: false,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1
+      }
+    }
+  ]
 });
